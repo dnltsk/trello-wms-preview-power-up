250a36d5fc88dace32b1981f2741884baa3a7520
danielt <daniel.teske@gmail.com>
2017-07-08 00:28:43 +0200

    disabled callbacks

--- client.js
+++ client.js
@@ -3,28 +3,28 @@ var enterGetCapabilitiesUrl = function (t) {
         title: 'Popup List Example',
         items: [
             {
-                text: 'Open Overlay',
-                callback: function (t) {
-                    return t.overlay({
-                        url: './overlay.html',
-                        args: {rand: (Math.random() * 100).toFixed(0)}
-                    })
-                        .then(function () {
-                            return t.closePopup();
-                        });
-                }
+                text: 'Open Overlay'
+                // ,callback: function (t) {
+                //     return t.overlay({
+                //         url: './overlay.html',
+                //         args: {rand: (Math.random() * 100).toFixed(0)}
+                //     })
+                //         .then(function () {
+                //             return t.closePopup();
+                //         });
+                // }
             },
             {
-                text: 'Open Board Bar',
-                callback: function (t) {
-                    return t.boardBar({
-                        url: './board-bar.html',
-                        height: 200
-                    })
-                        .then(function () {
-                            return t.closePopup();
-                        });
-                }
+                text: 'Open Board Bar'
+                // ,callback: function (t) {
+                //     return t.boardBar({
+                //         url: './board-bar.html',
+                //         height: 200
+                //     })
+                //         .then(function () {
+                //             return t.closePopup();
+                //         });
+                // }
             }
         ]
     });
