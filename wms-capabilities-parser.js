6007a0870f88da85e18d88e6d0a5d18a7ac77cef
danielt <daniel.teske@gmail.com>
2017-07-11 22:15:50 +0200

    true getmap

--- wms-capabilities-parser.js
+++ wms-capabilities-parser.js
@@ -3,23 +3,32 @@ var GetCapabilitiesParser = function () {
     this.parse = function (xmlBody) {
 
         var wmsCapabilities = {
-            'service': {
-                'name': $(xmlBody).find('Service').find('Name').text(),
-                'title': $(xmlBody).find('Service').find('Title').text()
+            service: {
+                name: $(xmlBody).find('Service').find('Name').text(),
+                title: $(xmlBody).find('Service').find('Title').text(),
+                getMap: parseGetMapCapabilities(xmlBody)
             }
         };
 
         var layers = [];
         $(xmlBody).find('Layer Layer').each(function () {
             layers.push({
-                'name': $(this).find("Name").text(),
-                'title': $(this).find("Title").text()
+                name: $(this).find("Name").text(),
+                title: $(this).find("Title").text()
             })
         });
 
         wmsCapabilities.layers = layers;
 
         return wmsCapabilities;
+    };
+
+    function parseGetMapCapabilities(xmlBody){
+        return {
+            resource : $(xmlBody).find('Capability Request GetMap OnlineResource[href]').text(),
+            format : $(xmlBody).find('Capability Request GetMap Format:first').text(),
+            crs : $(xmlBody).find('Capability Layer:first CRS:first').text()
+        }
     }
 
 
--- wms-url-builder.js
+++ wms-url-builder.js
@@ -1,6 +1,19 @@
 
 function createGetMapUrl(wmsCapabilities, layer) {
-    return 'http://129.206.228.72/cached/osm/service?service=wms&version=1.1.1&request=GetMap&layers=' + layer.name + '&styles=&srs=EPSG:4326&bbox=-180,-90,180,90&width=1024&height=512&format=image/png'
+    return [
+        wmsCapabilities.getMap.resource,
+        'SERVICE=WMS',
+        'VERSION=1.3.0',
+        'REQUEST=GetMap',
+        'LAYERS=' + layer.name,
+        'STYLES=',
+        'CRS=' + wmsCapabilities.getMap.crs,
+        'BBOX=-180,-90,180,90',
+        'width=1024',
+        'height=512',
+        'format='+wmsCapabilities.getMap.format
+    ].join('&');
+    //return 'http://129.206.228.72/cached/osm/service?service=wms&version=1.1.1&request=GetMap&layers=' + layer.name + '&styles=&srs=EPSG:4326&bbox=-180,-90,180,90&width=1024&height=512&format=image/png'
 }
 
 function createGetLegendGraphicUrl(wmsCapabilities, layer) {
