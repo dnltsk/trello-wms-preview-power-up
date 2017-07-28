c04cc9fbba48488c206e0b14692ec1caf51a1c5a
danielt <daniel.teske@gmail.com>
2017-07-29 00:16:03 +0200

    abstract

--- client.js
+++ client.js
@@ -14,92 +14,90 @@ var enterGetCapabilitiesUrl = function (t) {
         title: 'WMS Preview',
         items: [{
             text: 'attach Map..',
-            callback: new AttachPopup().attachMap
+            callback: attachMapPopup
         }, {
             text: 'attach LegendGraphic..',
-            callback: new AttachPopup().attachLegendGraphic
+            callback: attachLegendGraphicPopup
         }]
     });
 };
 
-var AttachPopup = function () {
-    this.attachMap = function (t, opts) {
-        return this._attachGenericPopup(t, opts);
-    };
 
-    this.attachLegendGraphic = function (t, opts) {
-        return this._attachGenericPopup(t, opts);
-    };
+attachMapPopup = function (t, opts) {
+    return this._attachGenericPopup(t, opts);
+};
 
-    this._attachGenericPopup = function (t, opts) {
-        console.log('_attachGenericPopup');
-        return t.popup({
-            title: 'Attach Map..',
-            items: function (t, options) {
-                var search = options.search;
-                if (!search || search.length === 0) {
-                    //no input
-                    return new Promise(function (resolve) {
-                        resolve([])
-                    });
-                }
-                if (search.trim().indexOf("https://") === -1) {
-                    //input does not start with https://
-                    return new Promise(function (resolve) {
-                        resolve([]);
-                    });
-                }
+attachLegendGraphicPopup = function (t, opts) {
+    return this._attachGenericPopup(t, opts);
+};
 
-                var jQueryPromise = $.ajax({
-                    url: search,
-                    dataType: "xml"
+attachGenericPopup = function (t, opts) {
+    console.log('_attachGenericPopup');
+    return t.popup({
+        title: 'Attach Map..',
+        items: function (t, options) {
+            var search = options.search;
+            if (!search || search.length === 0) {
+                //no input
+                return new Promise(function (resolve) {
+                    resolve([])
                 });
-                return jQueryPromise.then(
-                    function (xmlBody) {
-                        var wmsCapabilities = new GetCapabilitiesParser().parse(xmlBody);
-                        if (wmsCapabilities.version !== '1.3.0') {
-                            console.error('The WMS Service must be in Version 1.3.0 but is ' + wmsCapabilities.version);
-                            return new Promise(function (resolve) {
-                                resolve([]);
-                            });
-                        }
-                        var items = wmsCapabilities.layers.map(function (layer) {
-                            return {
-                                text: layer.title,
-                                callback: function () {
-                                    var getMapUrl = createGetMapUrl(wmsCapabilities, layer);
-                                    console.log("GetMap URL: ", getMapUrl);
-                                    t.attach({
-                                        name: 'GetMap: ' + layer.title,
-                                        url: getMapUrl
-                                    });
-                                }
-                            }
-                                ;
-                        });
-                        return new Promise(function (resolve) {
-                            resolve(items)
-                        });
-                    },
-                    function (error) {
-                        console.error('Unable to load GetCapabilities document: Does the resource contain CORS headers?');
+            }
+            if (search.trim().indexOf("https://") === -1) {
+                //input does not start with https://
+                return new Promise(function (resolve) {
+                    resolve([]);
+                });
+            }
+
+            var jQueryPromise = $.ajax({
+                url: search,
+                dataType: "xml"
+            });
+            return jQueryPromise.then(
+                function (xmlBody) {
+                    var wmsCapabilities = new GetCapabilitiesParser().parse(xmlBody);
+                    if (wmsCapabilities.version !== '1.3.0') {
+                        console.error('The WMS Service must be in Version 1.3.0 but is ' + wmsCapabilities.version);
                         return new Promise(function (resolve) {
                             resolve([]);
                         });
-                    })
+                    }
+                    var items = wmsCapabilities.layers.map(function (layer) {
+                        return {
+                            text: layer.title,
+                            callback: function () {
+                                var getMapUrl = createGetMapUrl(wmsCapabilities, layer);
+                                console.log("GetMap URL: ", getMapUrl);
+                                t.attach({
+                                    name: 'GetMap: ' + layer.title,
+                                    url: getMapUrl
+                                });
+                            }
+                        }
+                            ;
+                    });
+                    return new Promise(function (resolve) {
+                        resolve(items)
+                    });
+                },
+                function (error) {
+                    console.error('Unable to load GetCapabilities document: Does the resource contain CORS headers?');
+                    return new Promise(function (resolve) {
+                        resolve([]);
+                    });
+                })
 
-            },
-            search: {
-                // optional # of ms to debounce search to
-                // defaults to 300, override must be larger than 300
-                debounce: 300,
-                placeholder: 'Enter a WMS GetCapabilities URL',
-                empty: 'No Map Layers found',
-                searching: 'Searching for Map Layers...'
-            }
-        })
-    };
-
+        },
+        search: {
+            // optional # of ms to debounce search to
+            // defaults to 300, override must be larger than 300
+            debounce: 300,
+            placeholder: 'Enter a WMS GetCapabilities URL',
+            empty: 'No Map Layers found',
+            searching: 'Searching for Map Layers...'
+        }
+    })
 };
 
 
