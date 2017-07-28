f3c8dd8298d5716da2a36a1cc876ec6cc09c5a72
danielt <daniel.teske@gmail.com>
2017-07-28 22:48:29 +0200

    items

--- client.js
+++ client.js
@@ -3,26 +3,10 @@ var enterGetCapabilitiesUrl = function (t) {
             title: 'WMS Preview',
             items: [{
                 text: 'attach Map..',
-                callback: function (t, opts) {
-                    console.log('attach Map..', t, opts);
-                }
-                //, url: 'attachment-select-target.html'
+                url: 'attachment-select-target.html'
             }, {
                 text: 'attach LegendGraphic..',
-                callback: function (t, opts) {
-                    console.log('attach LegendGraphic..', t, opts);
-                    return t.popup({
-                        title: 'WMS Preview',
-                        items: [{
-                            text: 'enter foo..',
-                            callback: function (t, opts) {
-                                console.log('attach Foo..', t, opts);
-                            }
-                            //, url: 'attachment-select-target.html'
-                        }]
-                    })
-                }
-//, url: 'attachment-select-target.html'
+                url: 'attachment-select-target.html'
             }]
         })
             ;
