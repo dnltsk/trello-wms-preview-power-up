36642a039aa5445e948bb317b9c9139d2588ca5b
danielt <daniel.teske@gmail.com>
2017-07-28 22:47:15 +0200

    items

--- client.js
+++ client.js
@@ -1,21 +1,33 @@
 var enterGetCapabilitiesUrl = function (t) {
-    return t.popup({
-        title: 'WMS Preview',
-        items: [{
-            text: 'attach Map..',
-            callback: function (t, opts) {
-                console.log('attach Map..', t, opts);
-            }
-            //, url: 'attachment-select-target.html'
-        }, {
-            text: 'attach LegendGraphic..',
-            callback: function (t, opts) {
-                console.log('attach LegendGraphic..', t, opts);
-            }
-            //, url: 'attachment-select-target.html'
-        }]
-    });
-};
+        return t.popup({
+            title: 'WMS Preview',
+            items: [{
+                text: 'attach Map..',
+                callback: function (t, opts) {
+                    console.log('attach Map..', t, opts);
+                }
+                //, url: 'attachment-select-target.html'
+            }, {
+                text: 'attach LegendGraphic..',
+                callback: function (t, opts) {
+                    console.log('attach LegendGraphic..', t, opts);
+                    return t.popup({
+                        title: 'WMS Preview',
+                        items: [{
+                            text: 'enter foo..',
+                            callback: function (t, opts) {
+                                console.log('attach Foo..', t, opts);
+                            }
+                            //, url: 'attachment-select-target.html'
+                        }]
+                    })
+                }
+//, url: 'attachment-select-target.html'
+            }]
+        })
+            ;
+    }
+;
 
 TrelloPowerUp.initialize(
     {
