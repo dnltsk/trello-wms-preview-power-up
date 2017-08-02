cf3c2c5d7e915f130dc693a83946b8f2f19e4dc6
danielt <daniel.teske@gmail.com>
2017-08-02 17:36:21 +0200

    t.closePopup()

--- client.js
+++ client.js
@@ -85,17 +85,19 @@ attachGenericPopup = function (t, opts, attachmentMode) {
                                     t.attach({
                                         name: 'Map: ' + layer.title,
                                         url: getMapUrl
-                                    }).then(function(){
-                                        return t.closePopup();
-                                    });
+                                    })
+                                    //     .then(function(){
+                                    //     return t.closePopup();
+                                    // });
                                 } else {
                                     var getLegendGraphicUrl = createGetLegendGraphicUrl(wmsCapabilities, layer);
                                     t.attach({
                                         name: 'LegendGraphic: ' + layer.title,
                                         url: getLegendGraphicUrl
-                                    }).then(function(){
-                                        return t.closePopup();
-                                    });
+                                    })
+                                    //     .then(function(){
+                                    //     return t.closePopup();
+                                    // });
                                 }
                             }
                         };
